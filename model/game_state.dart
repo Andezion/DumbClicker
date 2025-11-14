f83515d41c3ed671fab1cd36dc3fcc57a079d718
Andezion <245122@edu.p.lodz.pl>
2025-11-14 10:58:30 +0100

    fixed constructor issue

--- model/game_state.dart
+++ model/game_state.dart
@@ -1,37 +1,57 @@
 class GameState {
-  double ects = 0.0;
-  double ectsPerClick = 0.1;
-  double ectsPerSecond = 0.0;
-  int semester = 1;
-  int totalEctsEarned = 0;
-  int prestigePoints = 0;
+  double ects;
+  double ectsPerClick;
+  double ectsPerSecond;
+  int semester;
+  int totalEctsEarned;
+  int prestigePoints;
 
-  String educationLevel = 'Licencjat'; // Licencjat, Magister, Doktorant, Profesor
-  int educationSemester = 1;
-  double motivation = 100.0;
-  DateTime lastMotivationUpdate = DateTime.now();
-  int battlePassLevel = 0;
-  int battlePassXP = 0;
-  bool isPremiumBattlePass = false;
-  List<String> unlockedSkins = ['default'];
-  String currentSkin = 'default';
+  String educationLevel;
+  int educationSemester;
+  double motivation;
+  DateTime lastMotivationUpdate;
+  int battlePassLevel;
+  int battlePassXP;
+  bool isPremiumBattlePass;
+  List<String> unlockedSkins;
+  String currentSkin;
   DateTime? lastEventTime;
 
-  Map<String, int> upgrades = {
-    'laptop': 0,
-    'coffee': 0,
-    'friend': 0,
-    'tutor': 0,
-    'earlyPass': 0,
+  Map<String, int> upgrades;
 
-    'dissertation': 0,
-    'scientificArticle': 0,
-    'conference': 0,
-
-    'grant': 0,
-    'laboratory': 0,
-    'publisher': 0,
-  };
+  GameState({
+    this.ects = 0.0,
+    this.ectsPerClick = 0.1,
+    this.ectsPerSecond = 0.0,
+    this.semester = 1,
+    this.totalEctsEarned = 0,
+    this.prestigePoints = 0,
+    this.educationLevel = 'Licencjat',
+    this.educationSemester = 1,
+    this.motivation = 100.0,
+    DateTime? lastMotivationUpdate,
+    this.battlePassLevel = 0,
+    this.battlePassXP = 0,
+    this.isPremiumBattlePass = false,
+    List<String>? unlockedSkins,
+    this.currentSkin = 'default',
+    this.lastEventTime,
+    Map<String, int>? upgrades,
+  })  : lastMotivationUpdate = lastMotivationUpdate ?? DateTime.now(),
+        unlockedSkins = unlockedSkins ?? ['default'],
+        upgrades = upgrades ?? {
+          'laptop': 0,
+          'coffee': 0,
+          'friend': 0,
+          'tutor': 0,
+          'earlyPass': 0,
+          'dissertation': 0,
+          'scientificArticle': 0,
+          'conference': 0,
+          'grant': 0,
+          'laboratory': 0,
+          'publisher': 0,
+        };
 
   int getRequiredEcts() {
     switch (educationLevel) {
@@ -57,7 +77,7 @@ class GameState {
       case 'Doktorant':
         return 4;
       case 'Profesor':
-        return 999;
+        return 999; // Endless
       default:
         return 7;
     }
@@ -84,28 +104,40 @@ class GameState {
   };
 
   factory GameState.fromJson(Map<String, dynamic> json) {
-    final state = GameState();
-    state.ects = json['ects'] ?? 0.0;
-    state.ectsPerClick = json['ectsPerClick'] ?? 0.1;
-    state.ectsPerSecond = json['ectsPerSecond'] ?? 0.0;
-    state.semester = json['semester'] ?? 1;
-    state.totalEctsEarned = json['totalEctsEarned'] ?? 0;
-    state.prestigePoints = json['prestigePoints'] ?? 0;
-    state.educationLevel = json['educationLevel'] ?? 'Licencjat';
-    state.educationSemester = json['educationSemester'] ?? 1;
-    state.motivation = json['motivation'] ?? 100.0;
-    state.lastMotivationUpdate = json['lastMotivationUpdate'] != null
-        ? DateTime.parse(json['lastMotivationUpdate'])
-        : DateTime.now();
-    state.battlePassLevel = json['battlePassLevel'] ?? 0;
-    state.battlePassXP = json['battlePassXP'] ?? 0;
-    state.isPremiumBattlePass = json['isPremiumBattlePass'] ?? false;
-    state.unlockedSkins = List<String>.from(json['unlockedSkins'] ?? ['default']);
-    state.currentSkin = json['currentSkin'] ?? 'default';
-    state.lastEventTime = json['lastEventTime'] != null
-        ? DateTime.parse(json['lastEventTime'])
-        : null;
-    state.upgrades = Map<String, int>.from(json['upgrades'] ?? state.upgrades);
-    return state;
+    return GameState(
+      ects: (json['ects'] ?? 0.0).toDouble(),
+      ectsPerClick: (json['ectsPerClick'] ?? 0.1).toDouble(),
+      ectsPerSecond: (json['ectsPerSecond'] ?? 0.0).toDouble(),
+      semester: json['semester'] ?? 1,
+      totalEctsEarned: json['totalEctsEarned'] ?? 0,
+      prestigePoints: json['prestigePoints'] ?? 0,
+      educationLevel: json['educationLevel'] ?? 'Licencjat',
+      educationSemester: json['educationSemester'] ?? 1,
+      motivation: (json['motivation'] ?? 100.0).toDouble(),
+      lastMotivationUpdate: json['lastMotivationUpdate'] != null
+          ? DateTime.parse(json['lastMotivationUpdate'])
+          : DateTime.now(),
+      battlePassLevel: json['battlePassLevel'] ?? 0,
+      battlePassXP: json['battlePassXP'] ?? 0,
+      isPremiumBattlePass: json['isPremiumBattlePass'] ?? false,
+      unlockedSkins: List<String>.from(json['unlockedSkins'] ?? ['default']),
+      currentSkin: json['currentSkin'] ?? 'default',
+      lastEventTime: json['lastEventTime'] != null
+          ? DateTime.parse(json['lastEventTime'])
+          : null,
+      upgrades: Map<String, int>.from(json['upgrades'] ?? {
+        'laptop': 0,
+        'coffee': 0,
+        'friend': 0,
+        'tutor': 0,
+        'earlyPass': 0,
+        'dissertation': 0,
+        'scientificArticle': 0,
+        'conference': 0,
+        'grant': 0,
+        'laboratory': 0,
+        'publisher': 0,
+      }),
+    );
   }
 }
